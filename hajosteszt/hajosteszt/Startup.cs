92025ba31ea14331436a501b4fcc250fbce017da
Brigitta Tóth <tbrigus@gmail.com>
2021-04-18 00:00:05 +0200

    8.gyakorlat

--- hajosteszt/hajosteszt/Startup.cs
+++ hajosteszt/hajosteszt/Startup.cs
@@ -18,6 +18,7 @@ namespace hajosteszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,13 +35,17 @@ namespace hajosteszt
 
             app.UseRouting();
 
-           // app.UseEndpoints(endpoints =>
-           // {
-             //   endpoints.MapGet("/", async context =>
-               // {
-                 //   await context.Response.WriteAsync("Hello World!");
-               // });
-           // });
+            // app.UseEndpoints(endpoints =>
+            // {
+            //   endpoints.MapGet("/", async context =>
+            // {
+            //   await context.Response.WriteAsync("Hello World!");
+            // });
+            // });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
